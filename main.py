883c135f46afb23cb7767cdd54b1f5faff927d41
Reload2704 <joranbra@espol.edu.ec>
2024-08-07 18:11:37 -0500

    Creacion de menú principal

---
+++ main.py
@@ -0,0 +1,54 @@
+# --------------- LIBRERIAS --------------- #
+# ----------------- MAIN ------------------ #
+
+opc = 0
+while(opc != 10): 
+    print(""" ----- MENÚ PRINCIPAL DE REFREEZER -----
+        1. Gestión de clientes
+        2. Gestión de proformas
+        3. Gestión de proyectos
+        4. Gestión de servicios
+        5. Gestión de empleados
+        6. Gestión de certificados
+        7. Gestión de materiales
+        8. Gestión de inventario
+        9. Gestión de proveedores
+        10. Salir
+    -----------------------------------------""")
+
+    opc = int(input("Ingrese una opcion: "))
+    
+    if (opc == 1):
+        print("\nOpcion 1")
+        input("Presione enter para continunar...\n")
+    
+    elif (opc == 2):
+        print("\nOpcion 2")
+        input("Presione enter para continunar...\n")
+    elif (opc == 3):
+        print("\nOpcion 3")
+        input("Presione enter para continunar...\n")
+    elif (opc == 4):
+        print("\nOpcion 4")
+        input("Presione enter para continunar...\n")
+    elif (opc == 5):
+        print("\nOpcion 5")
+        input("Presione enter para continunar...\n")
+    elif (opc == 6):
+        print("\nOpcion 6")
+        input("Presione enter para continunar...\n")
+    elif (opc == 7):
+        print("\nOpcion 7")
+        input("Presione enter para continunar...\n")
+    elif (opc == 8):
+        print("\nOpcion 8")
+        input("Presione enter para continunar...\n")
+    elif (opc == 9):
+        print("\nOpcion 9")
+        input("Presione enter para continunar...\n")
+    elif (opc == 10):
+        print("\nOpcion 10")
+        input("Presione enter para cerrar...\n")
+    else:
+        print("No existe esa opcion. Intente nuevamente")
+        input("Presione enter para continunar...\n")
