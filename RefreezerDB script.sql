d1cb34c9175c54e475600d6c34e4f8dfd4fd4ba0
Mapa <166883262+mapachavez@users.noreply.github.com>
2024-08-07 18:30:54 -0500

    Update RefreezerDB script.sql

--- RefreezerDB script.sql
+++ RefreezerDB script.sql
@@ -29,3 +29,20 @@ CREATE TABLE PROYECTO (
     fecha_inicio DATE NOT NULL,
     fecha_fin DATE
 );
+CREATE TABLE PROFORMA (
+    ID_Proforma INT auto_increment PRIMARY KEY,
+    fecha_emision DATE NOT NULL,
+    costo_mano_obra DECIMAL(10, 2) NOT NULL,
+    subtotal DECIMAL(10, 2) NOT NULL,
+    estado_aprobacion VARCHAR(50) CHECK(estado_aprobacion like "Aprobado" OR estado_aprobacion like "No Aprobado") NOT NULL DEFAULT "No Aprobado",
+    calle VARCHAR(100) NOT NULL,
+    manzana VARCHAR(50) NOT NULL,
+    ciudad VARCHAR(100) NOT NULL,
+    visita_fecha DATE NOT NULL,
+    visita_hora TIME NOT NULL,
+    visita_observacion TEXT,
+    ID_Proyecto INT NOT NULL,
+    ID_Cliente INT NOT NULL,
+    FOREIGN KEY (ID_Proyecto) REFERENCES PROYECTO(ID_Proyecto),
+    FOREIGN KEY (ID_Cliente) REFERENCES CLIENTE(ID_Cliente)
+);
