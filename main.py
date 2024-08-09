6b5e9b17ab7fac2faa474cf4715c9a0ba08f1753
Reload2704 <joranbra@espol.edu.ec>
2024-08-09 02:55:49 -0500

    Modificar datos de clientes con MySQL

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def gestionClientes(conexion):
     opc = "0"
     while (opc != "5"):
         cursor = conexion.cursor()
-        print("1. Insertar datos")
+        print("\n1. Insertar datos")
         print("2. Consultar datos")
         print("3. Modificar datos")
         print("4. Eliminar datos")
@@ -125,6 +125,101 @@ def gestionClientes(conexion):
                     ocon = ""
                     
             cursor.close()
+        if opc == "3":
+            while True:
+                print("\nSeleccione el tipo de cliente:")
+                print("1. Cliente Natural")
+                print("2. Cliente Empresa")
+                
+                tipo_cliente = input("\nIngrese el número correspondiente al tipo de cliente: ")
+                
+                if tipo_cliente in ['1', '2']:
+                    break
+                else:
+                    print("Selección inválida. Por favor, intente de nuevo.")
+            
+            id_cliente = input("Ingrese el ID del cliente: ")
+            
+            # Selección de la tabla a modificar
+            while True:
+                print("\nSeleccione la tabla que desea modificar:")
+                print("1. Tabla CLIENTE")
+                print("2. Tabla CLIENTE_NATURAL o CLIENTE_EMPRESA")
+                
+                tabla_seleccionada = input("Ingrese el número correspondiente a la tabla: ")
+                
+                if tabla_seleccionada in ['1', '2']:
+                    break
+                else:
+                    print("Selección inválida. Por favor, intente de nuevo.")
+                    
+            if tabla_seleccionada == '1':
+                # Modificación en la tabla CLIENTE
+                while True:
+                    print("\nSeleccione el campo a modificar en la tabla CLIENTE:")
+                    print("1. Nombre")
+                    print("2. Dirección")
+                    print("3. Correo")
+                    print("4. Teléfono")
+                    
+                    campo_seleccionado = input("Ingrese el número correspondiente al campo: ")
+                    
+                    campos = {
+                        '1': 'nombre',
+                        '2': 'direccion',
+                        '3': 'correo',
+                        '4': 'telefono'
+                    }
+                    
+                    campo = campos.get(campo_seleccionado)
+                    
+                    if campo:
+                        nuevo_valor = input(f"Ingrese el nuevo valor para {campo}: ")
+                        consulta = f"UPDATE CLIENTE SET {campo} = %s WHERE ID_cliente = %s"
+                        cursor.execute(consulta, (nuevo_valor, id_cliente))
+                        print(f"\nRegistro en la tabla CLIENTE con ID {id_cliente} actualizado correctamente.")
+                        break
+                    else:
+                        print("Selección de campo inválida. Por favor, intente de nuevo.")
+            
+            elif tabla_seleccionada == '2':
+                # Modificación en la tabla CLIENTE_NATURAL o CLIENTE_EMPRESA
+                if tipo_cliente == '1':
+                    while True:
+                        print("\nSeleccione el campo a modificar en la tabla CLIENTE_NATURAL:")
+                        print("1. Cédula")
+                        
+                        seleccion = input("Ingrese el número correspondiente al campo: ")
+                        
+                        if seleccion == '1':
+                            campo = 'Cedula'
+                            tabla = 'CLIENTE_NATURAL'
+                            break
+                        else:
+                            print("Selección de campo inválida. Por favor, intente de nuevo.")
+                
+                elif tipo_cliente == '2':
+                    while True:
+                        print("\nSeleccione el campo a modificar en la tabla CLIENTE_EMPRESA:")
+                        print("1. RUC")
+                        
+                        seleccion = input("Ingrese el número correspondiente al campo: ")
+                        
+                        if seleccion == '1':
+                            campo = 'RUC'
+                            tabla = 'CLIENTE_EMPRESA'
+                            break
+                        else:
+                            print("Selección de campo inválida. Por favor, intente de nuevo.")
+
+                nuevo_valor = input(f"Ingrese el nuevo valor para {campo}: ")
+                consulta = f"UPDATE {tabla} SET {campo} = %s WHERE ID_cliente = %s"
+                cursor.execute(consulta, (nuevo_valor, id_cliente))
+                print(f"\nRegistro en la tabla {tabla} con ID {id_cliente} actualizado correctamente.")
+            #Aplicar los cambios
+            conexion.commit()
+            cursor.close()
+            input("Aplicando cambios... \nPulse enter para continuar...")
 # ----------------- MAIN ------------------ #
 
 conexion = conectar() 
